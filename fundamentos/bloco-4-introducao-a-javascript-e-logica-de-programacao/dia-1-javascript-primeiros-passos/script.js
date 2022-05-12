41fd00d9c1f8e35cb7e80abe7182f766956a1f63
lucasrodrigges <rodrigges@Lucass-iMac-Pro.local>
2022-05-12 19:34:36 -0300

    resolução dos 5 primeiros exercicios

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -0,0 +1,85 @@
+// PROGRAMA 1
+const a = 5;
+const b = 2;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+
+// PROGRAMA 2
+const numA = 5;
+const numB = -10;
+
+if (numA > numB) {
+  console.log(numA);
+}
+else if (numB > numA) {
+  console.log(numB);
+}
+else {
+  console.log("São Iguais");
+}
+
+// PROGRAMA 3
+const A = 3000;
+const B = 90;
+const C = 500;
+
+if(A > B && A > C) {
+  console.log(A);
+}
+else if(B > A && B > C) {
+  console.log(B);
+}
+else {
+  console.log(C);
+}
+
+// PROGRAMA 4
+const number = -5;
+
+if(number > 0) {
+  console.log("Posiive");
+}
+else if(number < 0) {
+  console.log("Negative");
+} 
+else {
+  console.log("Zero");
+}
+
+// PROGRAMA 5
+const angle1 = -100;
+const angle2 = 40;
+const angle3 = 40;
+let soma = angle1 + angle2 + angle3;
+
+if(soma === 180 && angle1 >= 0 && angle2 >=0 && angle3 >= 0) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+// PROGRAMA 6
+
+
+// PROGRAMA 7
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
