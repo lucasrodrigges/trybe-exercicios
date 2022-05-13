d9e4af300e0ccdf8c7d1287b6efc51ef1ebad94b
lucasrodrigges <rodrigges@Lucass-iMac-Pro.local>
2022-05-13 03:41:15 -0300

    finalizando exercicios 4.1

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -62,7 +62,24 @@ if(soma === 180 && angle1 >= 0 && angle2 >=0 && angle3 >= 0) {
   console.log(false);
 }
 
+
 // PROGRAMA 6
+let piece = "ROOK";
+let lowerCase = piece.toLowerCase();
+
+if(lowerCase == "king" || lowerCase == "queen"){
+  console.log("Any Direction");
+} else if(lowerCase == "bishop"){
+  console.log("Diagonals");
+} else if(lowerCase == "rook"){
+  console.log("Horizontally or Vertically");
+} else if(lowerCase == "knights"){
+  console.log("L Shape");
+}else if(lowerCase== "pawns"){
+  console.log("vertically forward");
+} else{
+  console.log("Sorry. This piece doesn't exist.")
+}
 
 
 // PROGRAMA 7
@@ -82,19 +99,67 @@ if(porcentagem >= 90){
   console.log("Nota Inválida");
 }
 
+// PROGRAMA 8
+const firstNumber = 3;
+const secondNumber = 1;
+const thirdNumber = 8;
+
+if(firstNumber % 2 == 0 || secondNumber % 2 == 0 || thirdNumber % 2 == 0){
+  console.log("True");
+} else {
+  console.log("false");
+}
+
+// PROGRAMA 9
+const numberOne = 44;
+const numberTwo = 2;
+const numberThree = 99;
+
+if(numberOne % 2 == 1 || numberTwo % 2 == 1 || numberThree % 2 == 1){
+  console.log("True");
+} else {
+  console.log("false");
+}
+
+// PROGRAMA 10
+let valorCusto = 5;
+let valorVenda = 15;
+let valorCustoTotal = valorCusto + (0.2 * valorCusto);
+
+if(valorCusto >= 0 && valorVenda >= 0){
+  console.log(valorVenda - valorCustoTotal);  
+} else{
+  console.log("Revise suas contas aí, amigo. Valor incorreto.")
+}
+
+// PROGRAMA 11
+let salarioBruto = 3000;
+let salarioLiquido1;
+let tristezaTotal;
 
 
+if(salarioBruto <= 1556.94){
+  salarioLiquido1 = salarioBruto - 0.08 * salarioBruto;
+} else if(salarioBruto >= 1556.95 && salarioBruto <= 2594.92){
+  salarioLiquido1 = salarioBruto - 0.09 * salarioBruto;
+} else if(salarioBruto >= 2594.93 && salarioBruto <= 5189.82){
+  salarioLiquido1 = salarioBruto - 0.11 * salarioBruto;
+} else{
+  salarioLiquido1 = salarioBruto - 570.88;
+} 
 
+if(salarioLiquido1 <= 1903.98){
+  tristezaTotal = salarioLiquido1;
+} else if(salarioLiquido1 >= 1903.99 && salarioLiquido1 <= 2823.65){
+  tristezaTotal = salarioLiquido1 - 0.075 * salarioLiquido1 + 142.80;
+} else if(salarioLiquido1 >= 2826.66 && salarioLiquido1 <= 4664.68){
+  tristezaTotal = salarioLiquido1 - (0.15 * salarioLiquido1) + 354.80;
+} else{
+  tristezaTotal = salarioLiquido1 - (0.275 * salarioLiquido1) + 869,36;
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
+if(salarioLiquido1 <= 1903.98){
+  console.log(salarioLiquido1);
+} else {
+  console.log(tristezaTotal);
+}
