5c64e3d03dabc3ff24edece66d3270ce5a0d558b
lucasrodrigges <lucas.254262@gmail.com>
2022-05-17 13:03:02 -0300

    FIZ MAIS DUAS SEM CHORAR TANTO QUANTO NA PRIMEIRA

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
@@ -1,8 +1,8 @@
 // DESAFIO
-const n = 9 ;
+const n = 9;
 
 let resultado = 100;
-for(let i = 0; i <= n; i += 1){
+for (let i = 0; i <= n; i += 1) {
   resultado -= i;
 }
 console.log(resultado);
@@ -11,25 +11,61 @@ console.log(resultado);
 const number = 5;
 let result = 0;
 
-for(let j = 0; j <= number; j += 1){
+for (let j = 0; j <= number; j += 1) {
   result += j;
 }
 console.log(result);
 
 // EXERCÍCIO 1
-let value = 10;
-let parcialFatorial;
-let fatorial;
+// let value = 10;
+// let parcialFactorial;
+// let factorial;
 
-for(let i = value -1; i >= 1; i -=1){
-  parcialFatorial = value * i;
-  fatorial = parcialFatorial 
-  for(i >= 1; i -=1;){
-    fatorial *= i;
-    value -= 1;
-  }
-    
+// for(let i = value -1; i >= 1; i -=1){
+//   parcialFactorial = value * i;
+//   factorial = parcialFactorial 
+//   for(i >= 1; i -=1;){
+//     factorial *= i;
+//     value -= 1;
+//   }
+// }
+// console.log(factorial);
+
+// EXERCICIO 1 REFATORADO
+let num = 4;
+let factoring = 1;
+
+for (let i = factoring; i <= num; i += 1) {
+  factoring *= i
 }
-console.log(fatorial);
+console.log(factoring);
 
 // EXERCÍCIO 2
+// let word = 'tryber';
+// let array = [];
+// let newWord = '';
+// for(let i = word.length -1; i >= 0; i--){
+//   array.push(word[i]);
+// }
+// for(let i = 0; i < array.length; i++){
+//   newWord += array[i]
+// }
+// console.log(newWord);
+
+// EXERCÍCIO 2 REFATORADO
+let word = 'tryber';
+
+
+// EXERCÍCIO 3
+let arraySkill = ['java', 'javascript', 'python', 's', 'html', 'css' ];
+let higherWord = arraySkill[0];
+let smallerWord = arraySkill[0];
+
+for(let i = 1; i < arraySkill.length; i++){
+  if(arraySkill[i].length > higherWord.length){
+    higherWord = arraySkill[i];
+  } else if(arraySkill[i].length < smallerWord.length){
+    smallerWord = arraySkill[i];    
+  }
+}
+console.log(higherWord, smallerWord);
